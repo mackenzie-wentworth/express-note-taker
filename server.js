aee0541911c3e99f3c75829ad7a22bcfce837913
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-10 15:01:01 -0400

    added middleware, api and html routes, and app listener for PORT 3001 to server.js file

--- server.js
+++ server.js
@@ -7,3 +7,21 @@ const util = require('util');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Middleware for parsing JSON and urlencoded data for 'Public' folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// Routing for api and html
+const apiRouter = require('./routes/apiRoutes');
+const htmlRouter = require('./routes/htmlRoutes');
+
+app.use('/api/apiRoutes', apiRouter);
+app.use('/html/htmlRoutes', htmlRouter);
+
+// TODO: Create app listener for PORT
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
