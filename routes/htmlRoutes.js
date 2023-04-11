8728cb746a99c2504fed07daba46e04e6211f6ee
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-11 14:35:09 -0400

    added initial content to htmlRoutes.js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,17 @@
 // TODO: Import file packages
+const express = require('express');
+const path = require('path');
 
-// TODO: Create 'GET/notes' to return the notes.html file
+module.exports = (app) => {
+
+   // TODO: Create 'GET/notes' to return the notes.html file
+    app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+  
 // TODO: Create 'Get *' to return the index.html file
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    })
+  };
+
